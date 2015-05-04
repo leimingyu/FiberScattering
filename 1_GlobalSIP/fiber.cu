7e05b65012bae4ec886fffbe1f4701824557bc48
Leiming Yu <leimingyu830@gmail.com>
2015-05-04 11:20:25 -0400

    tmp

--- 1_GlobalSIP/fiber.cu
+++ 1_GlobalSIP/fiber.cu
@@ -43,6 +43,11 @@ d_atomN[9]={ 12.2126,  3.13220,  2.01250,  1.16630,
              0.005700, 9.89330, 28.9975,  0.582600, 
 			 -11.52};
 
+__device__ __constant__ char   atom_type_const[60000];
+
+texture<float4, 1, cudaReadModeElementType> crdc_tex;
+texture<float4, 1, cudaReadModeElementType> crdh_tex;
+texture<float4, 1, cudaReadModeElementType> crdo_tex;
 
 // global value
 char     *fname;
@@ -51,12 +56,18 @@ float     distance;
 int       span;
 int       nstreams;
 int       linenum;
+int       line_c;
+int       line_h;
+int       line_o;
 
 // unified memory
 float     *q;
 float4    *formfactor;
-char      *atom_type;
-float4    *crd;
+// char      *atom_type;
+
+float4    *crd_c;
+float4    *crd_h;
+float4    *crd_o;
 
 // event timing
 cudaEvent_t start, stop;
@@ -67,7 +78,6 @@ cudaStream_t *streams;
 
 //std::vector<char>  atom_type;
 //std::vector<float4> crd;
-
 
 void Usage(char *argv0)
 {
@@ -82,7 +92,6 @@ void Usage(char *argv0)
 	exit(-1);
 }
 
-// fixeme: bug!!!
 void readpdb()
 {
 	// Steps:
@@ -99,32 +108,75 @@ void readpdb()
 	if(fp == NULL)
 		perror("Error opening file!!!\n\n");
 
+	char s;
 	linenum = 0;
+	line_c  = 0;
+	line_h  = 0;
+	line_o  = 0;
+
 	while (fgets(line,1000,fp)!=NULL)
 	{
 		sscanf(line, "%s", c1);
 		if(!(strcmp(c1, "ATOM")))
-			linenum++;
+		{
+			linenum++;                               // atom list length
+			sscanf(line, "%*s %*d %s", atominfo);
+			s = atominfo[0];
+			if(s == 'C')
+				line_c++;
+			if(s == 'H')
+				line_h++;
+			if(s == 'O')
+				line_o++;
+		}
 	}
 
 	rewind(fp);
 
-	// unified memory 
-	cudaMallocManaged((void**)&atom_type,      sizeof(char) * linenum);
-	cudaMallocManaged((void**)&crd,          sizeof(float4) * linenum);
+//	std::cout << linenum << std::endl;
+	std::cout << line_c << std::endl;
+	std::cout << line_h << std::endl;
+	std::cout << line_o << std::endl;
 
-	int id = 0;
+	// unified memory 
+	// cudaMallocManaged((void**)&atom_type,      sizeof(char) * linenum);
+	//cudaMallocManaged((void**)&crd,          sizeof(float4) * linenum);
+
+	// unified memory
+	cudaMallocManaged((void**)&crd_c, sizeof(float4) * line_c);
+	cudaMallocManaged((void**)&crd_h, sizeof(float4) * line_h);
+	cudaMallocManaged((void**)&crd_o, sizeof(float4) * line_o);
+
+	int id_c, id_h, id_o;
+	id_c = id_h = id_o = 0;
+
 	while (fgets(line,1000,fp)!=NULL)
 	{
 		sscanf(line, "%s", c1);
 		if(!(strcmp(c1, "ATOM")))
 		{
 			sscanf(line, "%*s %*d %s %*s %*d %f %f %f", atominfo, &x, &y, &z);
-			atom_type[id] = atominfo[0];
-			crd[id]       = make_float4(x, y, z, 0.f);
-			id++;
+			s = atominfo[0];
+			if(s == 'C')
+			{
+				crd_c[id_c] = make_float4(x, y, z, 0.f);
+				id_c++;
+			}
+
+			if(s == 'H')
+			{
+				crd_h[id_h] = make_float4(x, y, z, 0.f);
+				id_h++;	
+			}
+
+			if(s == 'O')
+			{
+				crd_o[id_o] = make_float4(x, y, z, 0.f);
+				id_o++;
+			}
 		}
 	}
+
 	fclose(fp);
 }
 
@@ -284,6 +336,147 @@ int main(int argc, char*argv[])
 
 	readpdb();
 
+	// fixme: one thread doing the i/o
+	//        2nd thread working on the gpu prepare kernel
+
+
+	//-----------------------------------------------------------------------//
+	// plan 1 : pair wise compuation
+	//-----------------------------------------------------------------------//
+	//  cache the crd and atom_type in constant and texture memory
+	cudaChannelFormatDesc float4Desc = cudaCreateChannelDesc<float4>();
+	checkCudaErrors(cudaBindTexture(NULL, crdc_tex, crd_c, float4Desc));
+	checkCudaErrors(cudaBindTexture(NULL, crdh_tex, crd_h, float4Desc));
+	checkCudaErrors(cudaBindTexture(NULL, crdo_tex, crd_o, float4Desc));
+
+	// caculate all the combinations
+	// factorial(n) / (factorial(2) * factorial(n-2))
+	size_t cc = line_c * (line_c - 1) / 2;
+	size_t hh = line_h * (line_h - 1) / 2;
+	size_t oo = line_o * (line_o - 1) / 2;
+	size_t ch = line_c * line_h;
+	size_t co = line_c * line_o;
+	size_t ho = line_h * line_o;
+
+
+	// kernel
+
+	// kernel_cc
+	__global void kernel_cc()
+	{
+		size_t gid = __mul24(blockIdx.x, blockDim.x) + threadIdx.x;
+
+		if (gid < N)	
+		{
+			for		
+			
+		}
+	}
+
+/*
+	cudaMemcpyToSymbol(atom_type_const, 
+                       atom_type, 
+					   sizeof(char) * linenum, 
+					   0, 
+					   cudaMemcpyHostToDevice);
+
+	cudaChannelFormatDesc float4Desc = cudaCreateChannelDesc<float4>();
+	checkCudaErrors(cudaBindTexture(NULL, crd_tex, crd, float4Desc));
+*/
+
+/*
+	// slice the workloads for each stream
+	int atomNum = linenum; 
+	int lastpos = atomNum - 1;
+
+	int step = (atomNum - 1) / nstreams;
+
+	std::vector<int> stream_start;
+	std::vector<int> stream_en;
+
+	for(int i = 0; i < nstreams; i++)
+	{
+		stream_start.push_back(i * step);
+
+		if(i == (nstreams-1))
+		{
+			stream_end.push_back(atomNum-2);
+		}
+		else
+		{
+			stream_end.push_back((i + 1) * step - 1);
+		}
+	}
+
+
+	for(int sid = 0; sid < nstreams; sid++)
+	{
+		kernel_pairwise <<< grid, block, 0, streams[sid] >>> (q, 
+                                                              formfactor, 
+                                                              stream_start.at(i), 
+											                  stream_end.at(i), 
+											                  lastpos, 
+                                                              span, 
+															  sid)
+	}
+	*/
+
+/*
+__global__ void kernel_pairwise(
+                                
+                                const int start,
+                                const int end,
+                                const int lastpos,
+                                const int N,
+                                const int streamID)
+{
+	size_t gid = __mul24(blockIdx.x, blockDim.x) + threadIdx.x;
+
+	int offset = N * streamID;
+
+	if(gid < N)
+	{
+		for (int startpos = start; startpos <= end; ++startpos) 
+		{
+			char t1, t2;
+			float fj, fk;
+
+			float4 crd_ref = tex1Dfetch(crd_tex, startpos); // load coordinates
+
+			t1 = atom_type_const[startpos];                 // read d_atomtype 1 time, by all N threads
+
+			for(int i = startpos + 1; i <= lastpos; ++i)    // atoms to compare with the base atom
+			{
+				float4 cur_crd = tex1Dfetch(crd_tex, i);
+				float4 distance =  crd_ref - cur_crd;
+
+				t2 = atom_type_const[i];                    // read d_atomtype i times 
+				if (t2 == 'C')
+				{
+					 rzcc_xy = sqrtf(powf(distance.x, 2) + powf(distance.y, 2);
+					 rzcc_z  = abs(distance.z);
+
+				}
+				else if (t2 == 'H')
+				{
+
+				}
+				else if (t2 == 'O')
+				{
+				}
+				else
+				{}
+
+
+
+				iq  += fj_fk * j0(q * );
+
+			} // end of loop
+		}
+	}// end of if (gid < N)
+}
+*/
+
 //	for(int i=0; i<linenum; i++)
 //		printf("crd[%d] = %f\t%f\t%f\n", i, crd[i].x, crd[i].y, crd[i].z);		
 
@@ -295,10 +488,18 @@ int main(int argc, char*argv[])
 	//-----------------------------------------------------------------------//
 	// Free Resource
 	//-----------------------------------------------------------------------//
+	cudaUnbindTexture(crdc_tex);	
+	cudaUnbindTexture(crdh_tex);	
+	cudaUnbindTexture(crdo_tex);	
+
 	cudaFree(q);
 	cudaFree(formfactor);
-	cudaFree(atom_type);
-	cudaFree(crd);
+	cudaFree(crd_c);
+	cudaFree(crd_h);
+	cudaFree(crd_o);
+	// cudaFree(atom_type);
+
+	
 
 	for (int i = 0 ; i < nstreams ; i++){
 		cudaStreamDestroy(streams[i]);
