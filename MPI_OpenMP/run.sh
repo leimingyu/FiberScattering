d251843c6a6b01f34ff5bc09753a7181d3ef15c2
Leiming Yu <leimingyu830@gmail.com>
2019-04-23 00:04:51 -0400

    mpirun

--- MPI_OpenMP/run.sh
+++ MPI_OpenMP/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #cmd="mpiexec.mpich2 -np 4 ./fd --source ../pdbfiles/ec.pdb"
 #cmd="mpirun -np 1 ./fd --source ../pdbfiles/ec.pdb"
-cmd="mpirun -np 2 -machinefile hostlist-tcp ./fd --source ../pdbfiles/ec.pdb"
+#cmd="mpirun -np 2 -machinefile hostlist-tcp ./fd --source ../pdbfiles/ec.pdb"
+cmd="mpirun -np 64 ./fd --source ../data/gpgpu8/m1.pdb"
 $cmd
